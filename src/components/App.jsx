3c448e2388cdccc9221c97ea26595c84e85708f5
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-04-21 00:22:39 +0300

    Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,36 @@
-import { Toaster } from 'react-hot-toast';
+// import { Toaster } from 'react-hot-toast';
 
 import { Layout } from './Layout/Layout';
 
 import { AppWrapper } from './App.styled';
-import { App1 } from './App1';
 
 // import { useState, useEffect } from 'react';
 
-// import {
-//   Routes, // instead of "Switch"
-//   Route,
-// } from 'react-router-dom';
+import {
+  Routes, // instead of "Switch"
+  Route,
+  NavLink,
+} from 'react-router-dom';
 // import { BreedSelect } from './BreedSelect/BreedSelect';
 
 export const App = () => {
   return (
     <Layout>
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          duration: 1500,
-        }}
-      />
-
       <AppWrapper>
-        <App1 />
+        <ul>
+          <li>
+            <NavLink to="/">Home page</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dogs">Colection</NavLink>
+          </li>
+        </ul>
+        <Routes>
+          <Route exact path="/" element={<div>Home page</div>} />
+          <Route exact path="/dogs" element={<div>Dogs</div>} />
+          <Route exact path="/dogs/:dogId" element={<div>Dog</div>} />
+        </Routes>
       </AppWrapper>
-
-      {/* <Routes>
-        <Route exact path="/signup" element={<BreedSelect />} />
-      </Routes> */}
     </Layout>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import paintings from './painting.json';
 import { App } from 'components/App';
-// import { App1 } from 'components/App1';
-
-// import './index.css';
 import { GlobalStyle } from 'components/GlobalStyle';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    {/* <App1 /> */}
-
-    <GlobalStyle />
+    <BrowserRouter>
+      <App />
+      <GlobalStyle />
+    </BrowserRouter>
   </React.StrictMode>
 );
